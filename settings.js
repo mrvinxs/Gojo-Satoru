79468695d7e290ab1d6fa77e4e00588b80ee9682
mrvinxs <113854812+mrvinxs@users.noreply.github.com>
2022-09-20 00:19:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94760024170']
+global.premium = ['94760024170']
+global.ownernomer = '94760024170'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
